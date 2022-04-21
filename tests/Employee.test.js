ac2d2c0c5161258c2647bc5e7cfdd6aee19a8258
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-04-21 10:31:27 -0500

    Employee testing done

---
+++ tests/Employee.test.js
@@ -0,0 +1,33 @@
+const Employee = require('../lib/Employee');
+
+describe('Employee', () => {
+    const mockEmployee = {
+        name: 'Sally',
+        id: 130,
+        email: 'email@email.com',
+    };
+
+describe('constructor tests', () => {
+    test('should construct a new instance of an employee class', () => {
+        const employee = new Employee(mockEmployee);
+        expect(employee).toBeInstanceOf(Employee);
+    });
+});
+});
+
+describe('method tests', () => {
+    test('should return ID when the getID method is called', () => {
+        const employee = new Employee(mockEmployee);
+        expect(employee.getID()).toEqual(130);
+    });
+
+    test('should return employee name when getName method is called', () => {
+        const employee = new Employee(mockEmployee);
+        expect(employee.getName()).toEqual('Sally');
+    });
+
+    test('should return employee email when getEmail method is called', () => {
+        const employee = new Employee(mockEmployee);
+        expect(employee.getEmail()).toEqual('email@email.com');
+    });
+});
