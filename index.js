e2708e7616716d9bad9f23707ebe6ad12e0891e8
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-04-24 14:09:55 -0500

    fully working app

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const validateInput = (userInput) => {
   }
 };
 
-const init = async() => {
+const init = async () => {
   await createManager();
 
   let isTeamComplete = false;
@@ -45,20 +45,83 @@ const init = async() => {
 
   const generateHTML = (employees) => {
     const managers = employees.filter(manager => {
-      return manager.role === 'Manager';
+      return manager.getRole() === 'Manager';
     });
     const engineers = employees.filter(engineer => {
-      return engineer.role === 'Engineer';
-    }).map(engineer => {
-      return [engineer.name, engineer.email, engineer.id, engineer.github];
+      return engineer.getRole() === 'Engineer';
     });
-    console.log(engineers);
+    //console.log(engineers);
     const interns = employees.filter(intern => {
-      return intern.role === 'Intern';
-    }).map(intern => {
-      return [intern.name, intern.email, intern.id, intern.github]; 
+      return intern.getRole() === 'Intern';
     });
     //console.log(interns);
+    const managerHTML = managers.map(manager => {
+      return `
+      <div class="card">
+      <div class="card-body">
+          <h3 class = "card-title">${manager.getName()}</h3>
+          <h4 class = "card-subtitle mb-2">☕Manager</h4>
+          <div>ID: ${manager.getID()}</div>
+          <div>Email: <a href = "mailto: ${manager.getEmail()}">${manager.getEmail()}</a></div>
+          <div>Office Number: ${manager.getOfficeNumber()}</div>
+      </div>
+  </div>`
+    });
+
+    const engineerHTML = engineers.map(engineer => {
+      return `
+      <div class="card">
+      <div class="card-body">
+          <h3 class = "card-title">${engineer.getName()}</h3>
+          <h4 class = "card-subtitle mb-2">👓Engineer</h4>
+          <div>ID: ${engineer.getID()}</div>
+          <div>Email: <a href = "mailto: ${engineer.getEmail()}">${engineer.getEmail()}</a></div>
+          <div>GitHub: <a href = "https://github.com/${engineer.getGitHub()}">${engineer.getGitHub()}</a></div>
+      </div>
+  </div>`
+    });
+    const internHTML = interns.map(intern => {
+      return `
+      <div class="card">
+      <div class="card-body">
+          <h3 class = "card-title">${intern.getName()}</h3>
+          <h4 class = "card-subtitle mb-2">🎓Intern</h4>
+          <div>ID: ${intern.getID()}</div>
+          <div>Email: <a href = "mailto: ${intern.getEmail()}">${intern.getEmail()}</a></div>
+          <div>GitHub: <a href = "https://github.com/${intern.getGitHub()}">${intern.getGitHub()}</a></div>
+      </div>
+  </div>`
+    });
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css" />
+    <title>Team Profiles</title>
+</head>
+
+<body>
+    <div class="jumbotron">
+        <h1>Team Profile Generator</h1>
+    </div>
+
+    <div class = "card-deck">
+    ${managerHTML.join('\n')}
+    ${engineerHTML.join('\n')}
+    ${internHTML.join('\n')}
+        
+    </div>
+
+
+</body>
+
+</html>`
   };
 
   const HTML = generateHTML(employees);
@@ -71,7 +134,7 @@ const init = async() => {
   });
 };
 
-const createManager = async() => {
+const createManager = async () => {
   const managerQuestions = [
     {
       type: 'input',
@@ -106,7 +169,7 @@ const createManager = async() => {
   employees.push(newManager);
 };
 
-const createEngineer = async() => {
+const createEngineer = async () => {
 
   const engineerQuestions = [
     {
@@ -142,7 +205,7 @@ const createEngineer = async() => {
   employees.push(newEngineer);
 };
 
-const createIntern = async() => {
+const createIntern = async () => {
 
   const internQuestions = [
     {
